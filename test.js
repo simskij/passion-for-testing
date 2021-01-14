242d07cc1495322aa8d17cc64def5bd314b5df4b
Simon Aronsson <simme@arcticbit.se>
2021-01-14 18:27:57 +0100

    initial change

---
+++ test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { Rate } from 'k6/metrics';
+import { check } from 'k6';
+
+const failures = new Rate('failed requests');
+
+export const options = {
+  vus: 10,
+  duration: '10s',
+  thresholds: {
+    failed_requests: ['rate<=0'],
+    http_req_duration: ['p(95)<500']
+  }
+}
+
+export default function() {
+  const result = http.get('https://test-api.k6.io/');
+  check(result, {
+    'http response status code is 200': r => r.status === 200,
+  });
+  failures.add(result.status !== 200);
+}
